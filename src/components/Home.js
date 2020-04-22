b9c53134e67f61333205b42266a48f95308d9d84
tefiavalos <emlavalos92@gmail.com>
2020-04-22 19:53:59 -0300

    comienzo home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react';
+import img from '../assets/ipapate.jpg';
+import styled from 'styled-components';
+
+
+const HomePage = styled.section`
+display: flex;
+background-color: black;
+color: #eee;
+font-family: montserrat;
+img{
+    width: 40%;
+    height: auto;
+}
+article{
+    display: flex;
+    align-items: center;
+    text-align: center;
+}
+`
 
 const Home = () => {
     return(
-    <div className="main">
-        <img src="https://placekitten.com/200"></img>
-        <article><p>Hola, soy un texto</p></article>
-    </div>
+        <HomePage>
+        <img src={img}></img>
+        <article><p>Ipa es una gatita rescatada. Fue rescatada por dos personas que vieron como la tiraban
+            desde un auto adentro de una bolsa. Le dieron lugar, la cuidaron, y finalmente fue adoptada
+            por una pareja. Hoy el unico recuerdo de ese mal momento es el colmillo que le queda afuera por
+            tener la mandibula fracturada.</p></article>
+        </HomePage>
     )
 }
 
